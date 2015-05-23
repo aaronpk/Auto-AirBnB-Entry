6cd00a38db5330b00d21dc167b1e8b20642698b8
Aaron Parecki <aaron@parecki.com>
2015-05-23 22:28:54 +0000

    log multiple requests per sec

--- notify.php
+++ notify.php
@@ -6,8 +6,9 @@ $config = yaml_parse(file_get_contents('config.yml'));
 
 $input = json_decode(file_get_contents('php://input'));
 
-file_put_contents(dirname(__FILE__).'/logs/'.date('Ymd-His').'.txt', json_encode($input, JSON_PRETTY_PRINT));
+list($m,$s) = explode(' ',microtime());
+file_put_contents(dirname(__FILE__).'/logs/'.date('Ymd-His').'.'.round($m*1000000).'.txt', json_encode($input, JSON_PRETTY_PRINT));
 
 if(property_exists($input, type) && $input->type == 'CodeReport') {
-  send_irc_notification('', $input->locationName . ' door code was set to ' . $input->code);
+  send_irc_notification('', $input->locationName . ' door code was set to ' . $input->code . ' (index ' . $input->num . ')');
 }
