25d8df154673e704a9df076409f1b5c80710bb48
Aaron Parecki <aaron@parecki.com>
2015-05-23 21:55:30 +0000

    only send code change alert to irc since the smartapp sends to the phone directly

--- notify.php
+++ notify.php
@@ -9,5 +9,5 @@ $input = json_decode(file_get_contents('php://input'));
 file_put_contents(dirname(__FILE__).'/logs/'.date('Ymd-His').'.txt', json_encode($input, JSON_PRETTY_PRINT));
 
 if(property_exists($input, type) && $input->type == 'CodeReport') {
-  send_notification('', $input->locationName . ' door code was set to ' . $input->code);
+  send_irc_notification('', $input->locationName . ' door code was set to ' . $input->code);
 }
