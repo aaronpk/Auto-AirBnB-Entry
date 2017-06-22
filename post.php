5603f06a6badf6d716f263d88b374f3c4ee1400b
Aaron Parecki <aaron@parecki.com>
2017-06-22 22:54:24 +0000

    include third argument to set_door_code. fixes #1

--- post.php
+++ post.php
@@ -9,7 +9,7 @@ if($_POST['password'] == $config['master_pass']) {
     if($l['name'] == $_POST['location'])
       $location = $l;
   }
-  $result = set_door_code($location, $_POST['code']);  
+  $result = set_door_code($location, $_POST['code'], $location['code_index_0']);  
   echo "Okay!";
 } else {
   echo "Wrong password!";
